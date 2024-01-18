c405263b59ff2fafbbff4423424da4ac73faca81
mubashir6822 <119034258+mubashir6822@users.noreply.github.com>
2024-01-19 01:36:59 +0530

    Update File_organizerr.py

--- File_organizerr.py
+++ File_organizerr.py
@@ -6,33 +6,33 @@ def arrange_files(main_folder):
     
     moved_files_count = 0
     for file in files:
-        source_path = os.path.join(main_folder, file)
+        source_path = os.path.join(main_folder , file)
         if os.path.isfile(source_path):
             ext = file.split('.')[-1]
 
-            destination_path = os.path.join(main_folder, ext.title()) #if any below extention will not match mean it will create one new folder by using 3 letters of extention
+            destination_path = os.path.join(main_folder , ext.title()) #if any below extention will not match mean it will create one new folder by using 3 letters of extention
             
             if ext in ('txt'):
-                destination_path = os.path.join(main_folder, 'Textfiles')
-            elif ext in ('mp3', 'mp4', 'wav'):
-                destination_path = os.path.join(main_folder, 'Music_&_Vedios')
+                destination_path = os.path.join(main_folder,'Textfiles')
+            elif ext in ('mp3','mp4','wav'):
+                destination_path = os.path.join(main_folder,'Music_&_Vedios')
             elif ext in ('pdf'):
-                destination_path = os.path.join(main_folder, 'PDFfiles')
-            elif ext in ('doc', 'docx'):
-                destination_path = os.path.join(main_folder, 'Documents')
-            elif ext in ('jpg', 'png', 'jpeg', 'gif'):
-                destination_path = os.path.join(main_folder, 'Images')
+                destination_path = os.path.join(main_folder,'PDFfiles')
+            elif ext in ('doc','docx'):
+                destination_path = os.path.join(main_folder,'Documents')
+            elif ext in ('jpg','png','jpeg','gif'):
+                destination_path = os.path.join(main_folder ,'Images')
             
             base, ext = os.path.splitext(file)
             i = 0
             
-            while os.path.exists(os.path.join(destination_path, f"{base}({i}){ext}")):
+            while os.path.exists(os.path.join(destination_path,f"{base}({i}){ext}")):
                 i += 1
             
             new_destination_path = os.path.join(destination_path, f"{base}({i}){ext}")
 
             if os.path.exists(destination_path):
-                shutil.move(source_path, new_destination_path)
+                shutil.move(source_path,new_destination_path)
                 moved_files_count += 1
             else:
                 os.makedirs(destination_path)
@@ -40,10 +40,10 @@ def arrange_files(main_folder):
                 moved_files_count += 1
 
     if moved_files_count > 0:
-        print('Job is done successfully. Files are organized.')
-        print('Number of files organized:', moved_files_count)
+        print('Job is done successfully Files are organized.')
+        print('Number of files organized:' , moved_files_count)
     else:
-        print('No files are there to organize.')
+        print('No files are there to organize')
 
 
 while True:
@@ -51,6 +51,6 @@ while True:
     if os.path.exists(main_folder_path):
         break
     else:
-        print("Invalid main folder path. Please try again.")
+        print("Invalid main folder path Please try again")
     
 arrange_files(main_folder_path)
